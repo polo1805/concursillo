5234ff0c77478ed7d1d9b0d974b6a7d224c3d1c4
Alejandro <alejandroo.polo@gmail.com>
2025-09-17 18:09:25 +0200

    Creacion de componentes control y concursillo y de los servicios

---
+++ concursillo-front/src/app/pages/concurso/concurso.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-concurso',
+  standalone: true,
+  imports: [],
+  templateUrl: './concurso.component.html',
+  styleUrl: './concurso.component.css'
+})
+export class ConcursoComponent {
+
+}
